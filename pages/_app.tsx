bd516cfd6043d3fd362e8de919b3b57464dd56c1
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-06-02 00:36:09 +0330

    feat ( header ) : add header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import ResponsiveAppBar from "@/component/Header/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
